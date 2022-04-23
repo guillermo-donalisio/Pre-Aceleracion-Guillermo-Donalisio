df32a894a6b3cafe3b17ea75a62a8fc315f9463c
Guillermo Donalisio <guillermo.donalisio1@gmail.com>
2022-04-23 20:34:23 -0300

    token config

--- Api-Disney/Program.cs
+++ Api-Disney/Program.cs
@@ -1,8 +1,14 @@
+using System.Text;
 using Api_Disney.Data;
+using Api_Disney.Models.Auth;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
 
 // Add services to the container.
 
@@ -14,14 +20,64 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(o =>
 {
     o.SwaggerDoc("v1", new OpenApiInfo { Title = "Pre-Aceleracion Guillermo Donalisio", Version = "v1" });
+    o.AddSecurityDefinition("Bearer", 
+    new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+    });
+    o.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+			new List<string>()
+        }
+    });
 });
 
 // Create Database SQL SERVER
 var disneyConn = builder.Configuration.GetConnectionString("DisneyConnection");
 builder.Services.AddDbContext<DisneyContext>(x => x.UseSqlServer(disneyConn));
-
 var userConn = builder.Configuration.GetConnectionString("UserConnection");
 builder.Services.AddDbContext<UserContext>(x => x.UseSqlServer(userConn));
+
+// DI to implement the Login and Register system
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<UserContext>()
+    .AddDefaultTokenProviders();
+
+// DI to Configure token and Authentication
+builder.Services.AddAuthentication(option => 
+{
+    option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(option => 
+{
+    option.SaveToken = true;
+    option.RequireHttpsMetadata = false; // required in production environment
+    option.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = configuration["JWT:ValidAudience"], 
+        ValidIssuer = configuration["JWT:ValidIssuer"],        
+        IssuerSigningKey = 
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+    };
+});
 
 // Build API
 var app = builder.Build();
@@ -36,7 +92,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 // Authentication & Authorization
-//app.UseAuthentication();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
