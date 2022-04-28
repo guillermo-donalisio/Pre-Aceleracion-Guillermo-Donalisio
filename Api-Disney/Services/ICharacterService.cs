cdf44e4fe224054107165965acb9cf110c5b43ab
Guillermo Donalisio <guillermo.donalisio1@gmail.com>
2022-04-28 19:56:37 -0300

    service update

--- Api-Disney/Services/ICharacterService.cs
+++ Api-Disney/Services/ICharacterService.cs
@@ -4,4 +4,6 @@ namespace Api_Disney.Services;
 
 public interface ICharacterService : IGenericService<Character>
 {
+	IQueryable<Character> GetQuery();
+
 }
--- Api-Disney/Services/Implements/CharacterService.cs
+++ Api-Disney/Services/Implements/CharacterService.cs
@@ -11,4 +11,10 @@ public class CharacterService : GenericService<Character>, ICharacterService
     {
         this._characterRepository = characterRepository;
     }
+
+    public IQueryable<Character> GetQuery()
+    {
+        return _characterRepository.GetQuery();
+    }
+ 
 }
