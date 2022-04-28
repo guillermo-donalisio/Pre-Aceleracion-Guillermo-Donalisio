315960dce223da756421a06e63440bfbb4129ac9
Guillermo Donalisio <guillermo.donalisio1@gmail.com>
2022-04-28 19:50:22 -0300

    viewModels

---
+++ Api-Disney/ViewModels/CRUD/Characters/CreateCharacterDTO.cs
@@ -0,0 +1,10 @@
+namespace Api_Disney.ViewModels.CRUD.Characters;
+
+public class CreateCharacterDTO
+{
+    public string image_url {set;get;}
+    public string name {set;get;}
+    public int age {set;get;}
+    public decimal weight {set;get;}
+    public string story {set;get;}
+}
---
+++ Api-Disney/ViewModels/CRUD/Characters/ListCharacterDTO.cs
@@ -0,0 +1,7 @@
+namespace Api_Disney.ViewModels.CRUD.Characters;
+
+public class ListCharacterDTO
+{
+    public string img_url{set;get;}
+    public string name{set;get;}
+}
