46d15db25f8f1756fde99546d7435f3438799d48
Guillermo Donalisio <guillermo.donalisio1@gmail.com>
2022-04-29 21:02:35 -0300

    new character dto

---
+++ Api-Disney/ViewModels/CRUD/Characters/SearchCharacterDTO.cs
@@ -0,0 +1,9 @@
+namespace Api_Disney.ViewModels.CRUD.Characters;
+
+public class SearchCharacterDTO
+{
+    public string name {set;get;}
+    public int age {set;get;}
+    public decimal weight {set;get;}
+    public int movieId {set;get;}
+}
