46b68f50d136e761c696c519761ce8f02794c5c0
Guillermo Donalisio <guillermo.donalisio1@gmail.com>
2022-04-30 21:05:50 -0300

    new dto movies

---
+++ Api-Disney/ViewModels/CRUD/Movie/ListMovieDTO.cs
@@ -0,0 +1,8 @@
+namespace Api_Disney.ViewModels.CRUD.Movie;
+
+public class ListMovieDTO
+{
+    public string img_url{set;get;}
+    public string title{set;get;}
+    public DateTime date{set;get;}
+}
